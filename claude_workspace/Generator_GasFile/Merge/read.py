bd1d000ff57c88e7e056ec25423daa3288ae3948
JTYONG <jarrodyong03@gmail.com>
2026-05-19 12:45:32 +0800

    oldgastable_plot

---
+++ claude_workspace/Generator_GasFile/Merge/read.py
@@ -0,0 +1,32 @@
+import ROOT
+import Garfield
+
+# Set up the gas.
+gas = ROOT.Garfield.MediumMagboltz()
+gas.LoadGasFile('build/TIFRH_merged_40000_43000.gas')
+#gas.LoadIonMobility('IonMobility_Ar+_Ar.txt')
+gas.PrintGas()
+
+view = ROOT.Garfield.ViewMedium(gas)
+view.SetMagneticField(2.)
+  
+cV = ROOT.TCanvas('cV', '', 600, 600)
+view.SetCanvas(cV)
+view.PlotElectronVelocity()
+cV.SaveAs('plots/PlotElectronVelocity.png')
+
+cD = ROOT.TCanvas('cD', '', 600, 600)
+view.SetCanvas(cD)
+view.PlotElectronDiffusion()
+cV.SaveAs('plots/PlotElectronDiffusion.png')
+
+cT = ROOT.TCanvas('cT', '', 600, 600)
+view.SetCanvas(cT)
+view.PlotElectronTownsend()
+cV.SaveAs('plots/PlotElectronTownsend.png')
+
+
+cA = ROOT.TCanvas('cA', '', 600, 600)
+view.SetCanvas(cA)
+view.PlotElectronAttachment()
+cV.SaveAs('plots/PlotElectronAttachment.png')
