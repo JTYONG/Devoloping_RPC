f217298d4c720eadbc07769712f1dbb492aa6520
JTYONG <jarrodyong03@gmail.com>
2026-05-19 13:03:56 +0800

    oldGT_Plot

--- claude_workspace/Generator_GasFile/Merge/read.py
+++ claude_workspace/Generator_GasFile/Merge/read.py
@@ -20,14 +20,14 @@ cD = ROOT.TCanvas('cD', '', 600, 600)
 view.SetCanvas(cD)
 view.PlotElectronDiffusion()
 cD.Update()
-cV.SaveAs('plots/PlotElectronDiffusion.png')
+cD.SaveAs('plots/PlotElectronDiffusion.png')
 
 
 cT = ROOT.TCanvas('cT', '', 600, 600)
 view.SetCanvas(cT)
 view.PlotElectronTownsend()
 cT.Update()
-cV.SaveAs('plots/PlotElectronTownsend.png')
+cT.SaveAs('plots/PlotElectronTownsend.png')
 
 cA = ROOT.TCanvas('cA', '', 600, 600)
 view.SetCanvas(cA)
