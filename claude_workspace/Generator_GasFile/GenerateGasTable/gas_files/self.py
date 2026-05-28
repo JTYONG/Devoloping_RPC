30e4859a8ce4bf4a73ec57f19093e2e92df79c77
JT Yong <jarrodyong03@gmail.com>
2026-05-28 18:04:26 +0800

    a

--- claude_workspace/Generator_GasFile/GenerateGasTable/gas_files/self.py
+++ claude_workspace/Generator_GasFile/GenerateGasTable/gas_files/self.py
@@ -64,9 +64,26 @@ print(len(eff_alpha1))
 fig,ax = plt.subplots(2,2,figsize=(18,18))
 
 ax[0,0].plot(ef,V1,marker="x",color="blue")
+ax[0,0].set_title("Electron Drift Velocity")
+ax[0,0].set_xlabel("Efield [V/cm]")
+ax[0,0].set_ylabel("$V_D$ [cm/ns]")
+
+
 ax[0,1].plot(ef,alpha1,marker="x",color="blue")
+ax[0,1].set_title("Electron Townsend Coefficient")
+ax[0,1].set_xlabel("Efield [V/cm]")
+ax[0,1].set_ylabel("$alpha$ $[cm_{-1}]$")
+
+
 ax[1,0].plot(ef,eta1,marker="x",color="blue")
+ax[1,0].set_title("Electron Attachment Coefficient")
+ax[1,0].set_xlabel("Efield [V/cm]")
+ax[1,0].set_ylabel("$eta$ $[cm_{-1}]$")
+
 ax[1,1].plot(ef,eff_alpha1,marker="x",color="blue")
+ax[1,1].set_title("Electroon Effective Townsend Coefficient")
+ax[1,1].set_xlabel("Efield [V/cm]")
+ax[1,1].set_ylabel("$alpha_{eff}$ $[cm^{-1}]$")
 
 fig.savefig("transport coefficients")
 
