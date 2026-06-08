1346f4558e3cde95f1e9c91a9a96ec9e4ab82e67
JTYONG <jarrodyong03@gmail.com>
2026-06-09 00:36:54 +0800

    a

---
+++ FromDrNizam/build_ParallelPlate/build/CMakeFiles/ParallelPlate.dir/link.d
@@ -0,0 +1,181 @@
+ParallelPlate: \
+  /home/sus/miniforge3/envs/physics/bin/../x86_64-conda-linux-gnu/sysroot/usr/lib/../lib/Scrt1.o \
+  /home/sus/miniforge3/envs/physics/bin/../x86_64-conda-linux-gnu/sysroot/usr/lib/../lib/crti.o \
+  /home/sus/miniforge3/envs/physics/bin/../lib/gcc/x86_64-conda-linux-gnu/14.3.0/crtbeginS.o \
+  CMakeFiles/ParallelPlate.dir/ParallelPlate.C.o \
+  /home/sus/miniforge3/envs/physics/lib/libGarfield.so.0.3 \
+  /home/sus/miniforge3/envs/physics/lib/libTree.so.6.38.04 \
+  /home/sus/miniforge3/envs/physics/lib/libGdml.so.6.38.04 \
+  /home/sus/miniforge3/envs/physics/lib/libGeom.so.6.38.04 \
+  /home/sus/miniforge3/envs/physics/lib/libXMLIO.so.6.38.04 \
+  /home/sus/miniforge3/envs/physics/lib/libGraf3d.so.6.38.04 \
+  /home/sus/miniforge3/envs/physics/lib/libGpad.so.6.38.04 \
+  /home/sus/miniforge3/envs/physics/lib/libGraf.so.6.38.04 \
+  /home/sus/miniforge3/envs/physics/lib/libHist.so.6.38.04 \
+  /home/sus/miniforge3/envs/physics/lib/libMatrix.so.6.38.04 \
+  /home/sus/miniforge3/envs/physics/lib/libHeed.so \
+  /home/sus/miniforge3/envs/physics/lib/libGarfieldRandom.so \
+  /home/sus/miniforge3/envs/physics/lib/libMathCore.so.6.38.04 \
+  /home/sus/miniforge3/envs/physics/lib/libImt.so.6.38.04 \
+  /home/sus/miniforge3/envs/physics/lib/libMultiProc.so.6.38.04 \
+  /home/sus/miniforge3/envs/physics/lib/libNet.so.6.38.04 \
+  /home/sus/miniforge3/envs/physics/lib/libRIO.so.6.38.04 \
+  /home/sus/miniforge3/envs/physics/lib/libThread.so.6.38.04 \
+  /home/sus/miniforge3/envs/physics/bin/../x86_64-conda-linux-gnu/sysroot/usr/lib/../lib/libpthread.so \
+  /home/sus/miniforge3/envs/physics/bin/../x86_64-conda-linux-gnu/sysroot/usr/lib/../lib/libpthread.so \
+  /home/sus/miniforge3/envs/physics/bin/../x86_64-conda-linux-gnu/sysroot/usr/lib/../lib/libpthread.so \
+  /home/sus/miniforge3/envs/physics/bin/../x86_64-conda-linux-gnu/sysroot/lib64/libpthread.so.0 \
+  /home/sus/miniforge3/envs/physics/bin/../x86_64-conda-linux-gnu/sysroot/usr/lib64/libpthread_nonshared.a \
+  /home/sus/miniforge3/envs/physics/lib/libCore.so.6.38.04 \
+  /home/sus/miniforge3/envs/physics/lib/libMagboltz.so.11.19 \
+  /home/sus/miniforge3/envs/physics/lib/libneBEM.so \
+  /home/sus/miniforge3/envs/physics/lib/libDegrade.so.3.17 \
+  /home/sus/miniforge3/envs/physics/lib/libgomp.so \
+  /home/sus/miniforge3/envs/physics/bin/../x86_64-conda-linux-gnu/sysroot/usr/lib/../lib/libpthread.so \
+  /home/sus/miniforge3/envs/physics/bin/../x86_64-conda-linux-gnu/sysroot/usr/lib/../lib/libpthread.so \
+  /home/sus/miniforge3/envs/physics/bin/../x86_64-conda-linux-gnu/sysroot/usr/lib/../lib/libpthread.so \
+  /home/sus/miniforge3/envs/physics/bin/../x86_64-conda-linux-gnu/sysroot/lib64/libpthread.so.0 \
+  /home/sus/miniforge3/envs/physics/bin/../x86_64-conda-linux-gnu/sysroot/usr/lib64/libpthread_nonshared.a \
+  /home/sus/miniforge3/envs/physics/lib/libGarfieldGPU.a \
+  /home/sus/miniforge3/envs/physics/lib/libstdc++.so \
+  /home/sus/miniforge3/envs/physics/bin/../x86_64-conda-linux-gnu/sysroot/usr/lib/../lib/libm.so \
+  /home/sus/miniforge3/envs/physics/lib/libgcc_s.so \
+  /home/sus/miniforge3/envs/physics/lib/libgcc_s.so \
+  /home/sus/miniforge3/envs/physics/lib/libgcc_s.so \
+  /home/sus/miniforge3/envs/physics/lib/libgcc_s.so.1 \
+  /home/sus/miniforge3/envs/physics/bin/../lib/gcc/x86_64-conda-linux-gnu/14.3.0/libgcc.a \
+  /home/sus/miniforge3/envs/physics/bin/../lib/gcc/x86_64-conda-linux-gnu/14.3.0/libgcc.a \
+  /home/sus/miniforge3/envs/physics/bin/../x86_64-conda-linux-gnu/sysroot/usr/lib/../lib/libc.so \
+  /home/sus/miniforge3/envs/physics/bin/../x86_64-conda-linux-gnu/sysroot/usr/lib/../lib/libc.so \
+  /home/sus/miniforge3/envs/physics/bin/../x86_64-conda-linux-gnu/sysroot/usr/lib/../lib/libc.so \
+  /home/sus/miniforge3/envs/physics/bin/../x86_64-conda-linux-gnu/sysroot/lib64/libc.so.6 \
+  /home/sus/miniforge3/envs/physics/bin/../x86_64-conda-linux-gnu/sysroot/usr/lib64/libc_nonshared.a \
+  /home/sus/miniforge3/envs/physics/bin/../x86_64-conda-linux-gnu/sysroot/lib64/ld-linux-x86-64.so.2 \
+  /home/sus/miniforge3/envs/physics/lib/libgcc_s.so \
+  /home/sus/miniforge3/envs/physics/lib/libgcc_s.so \
+  /home/sus/miniforge3/envs/physics/lib/libgcc_s.so \
+  /home/sus/miniforge3/envs/physics/lib/libgcc_s.so.1 \
+  /home/sus/miniforge3/envs/physics/bin/../lib/gcc/x86_64-conda-linux-gnu/14.3.0/libgcc.a \
+  /home/sus/miniforge3/envs/physics/bin/../lib/gcc/x86_64-conda-linux-gnu/14.3.0/libgcc.a \
+  /home/sus/miniforge3/envs/physics/bin/../lib/gcc/x86_64-conda-linux-gnu/14.3.0/crtendS.o \
+  /home/sus/miniforge3/envs/physics/bin/../x86_64-conda-linux-gnu/sysroot/usr/lib/../lib/crtn.o
+
+/home/sus/miniforge3/envs/physics/bin/../x86_64-conda-linux-gnu/sysroot/usr/lib/../lib/Scrt1.o:
+
+/home/sus/miniforge3/envs/physics/bin/../x86_64-conda-linux-gnu/sysroot/usr/lib/../lib/crti.o:
+
+/home/sus/miniforge3/envs/physics/bin/../lib/gcc/x86_64-conda-linux-gnu/14.3.0/crtbeginS.o:
+
+CMakeFiles/ParallelPlate.dir/ParallelPlate.C.o:
+
+/home/sus/miniforge3/envs/physics/lib/libGarfield.so.0.3:
+
+/home/sus/miniforge3/envs/physics/lib/libTree.so.6.38.04:
+
+/home/sus/miniforge3/envs/physics/lib/libGdml.so.6.38.04:
+
+/home/sus/miniforge3/envs/physics/lib/libGeom.so.6.38.04:
+
+/home/sus/miniforge3/envs/physics/lib/libXMLIO.so.6.38.04:
+
+/home/sus/miniforge3/envs/physics/lib/libGraf3d.so.6.38.04:
+
+/home/sus/miniforge3/envs/physics/lib/libGpad.so.6.38.04:
+
+/home/sus/miniforge3/envs/physics/lib/libGraf.so.6.38.04:
+
+/home/sus/miniforge3/envs/physics/lib/libHist.so.6.38.04:
+
+/home/sus/miniforge3/envs/physics/lib/libMatrix.so.6.38.04:
+
+/home/sus/miniforge3/envs/physics/lib/libHeed.so:
+
+/home/sus/miniforge3/envs/physics/lib/libGarfieldRandom.so:
+
+/home/sus/miniforge3/envs/physics/lib/libMathCore.so.6.38.04:
+
+/home/sus/miniforge3/envs/physics/lib/libImt.so.6.38.04:
+
+/home/sus/miniforge3/envs/physics/lib/libMultiProc.so.6.38.04:
+
+/home/sus/miniforge3/envs/physics/lib/libNet.so.6.38.04:
+
+/home/sus/miniforge3/envs/physics/lib/libRIO.so.6.38.04:
+
+/home/sus/miniforge3/envs/physics/lib/libThread.so.6.38.04:
+
+/home/sus/miniforge3/envs/physics/bin/../x86_64-conda-linux-gnu/sysroot/usr/lib/../lib/libpthread.so:
+
+/home/sus/miniforge3/envs/physics/bin/../x86_64-conda-linux-gnu/sysroot/usr/lib/../lib/libpthread.so:
+
+/home/sus/miniforge3/envs/physics/bin/../x86_64-conda-linux-gnu/sysroot/usr/lib/../lib/libpthread.so:
+
+/home/sus/miniforge3/envs/physics/bin/../x86_64-conda-linux-gnu/sysroot/lib64/libpthread.so.0:
+
+/home/sus/miniforge3/envs/physics/bin/../x86_64-conda-linux-gnu/sysroot/usr/lib64/libpthread_nonshared.a:
+
+/home/sus/miniforge3/envs/physics/lib/libCore.so.6.38.04:
+
+/home/sus/miniforge3/envs/physics/lib/libMagboltz.so.11.19:
+
+/home/sus/miniforge3/envs/physics/lib/libneBEM.so:
+
+/home/sus/miniforge3/envs/physics/lib/libDegrade.so.3.17:
+
+/home/sus/miniforge3/envs/physics/lib/libgomp.so:
+
+/home/sus/miniforge3/envs/physics/bin/../x86_64-conda-linux-gnu/sysroot/usr/lib/../lib/libpthread.so:
+
+/home/sus/miniforge3/envs/physics/bin/../x86_64-conda-linux-gnu/sysroot/usr/lib/../lib/libpthread.so:
+
+/home/sus/miniforge3/envs/physics/bin/../x86_64-conda-linux-gnu/sysroot/usr/lib/../lib/libpthread.so:
+
+/home/sus/miniforge3/envs/physics/bin/../x86_64-conda-linux-gnu/sysroot/lib64/libpthread.so.0:
+
+/home/sus/miniforge3/envs/physics/bin/../x86_64-conda-linux-gnu/sysroot/usr/lib64/libpthread_nonshared.a:
+
+/home/sus/miniforge3/envs/physics/lib/libGarfieldGPU.a:
+
+/home/sus/miniforge3/envs/physics/lib/libstdc++.so:
+
+/home/sus/miniforge3/envs/physics/bin/../x86_64-conda-linux-gnu/sysroot/usr/lib/../lib/libm.so:
+
+/home/sus/miniforge3/envs/physics/lib/libgcc_s.so:
+
+/home/sus/miniforge3/envs/physics/lib/libgcc_s.so:
+
+/home/sus/miniforge3/envs/physics/lib/libgcc_s.so:
+
+/home/sus/miniforge3/envs/physics/lib/libgcc_s.so.1:
+
+/home/sus/miniforge3/envs/physics/bin/../lib/gcc/x86_64-conda-linux-gnu/14.3.0/libgcc.a:
+
+/home/sus/miniforge3/envs/physics/bin/../lib/gcc/x86_64-conda-linux-gnu/14.3.0/libgcc.a:
+
+/home/sus/miniforge3/envs/physics/bin/../x86_64-conda-linux-gnu/sysroot/usr/lib/../lib/libc.so:
+
+/home/sus/miniforge3/envs/physics/bin/../x86_64-conda-linux-gnu/sysroot/usr/lib/../lib/libc.so:
+
+/home/sus/miniforge3/envs/physics/bin/../x86_64-conda-linux-gnu/sysroot/usr/lib/../lib/libc.so:
+
+/home/sus/miniforge3/envs/physics/bin/../x86_64-conda-linux-gnu/sysroot/lib64/libc.so.6:
+
+/home/sus/miniforge3/envs/physics/bin/../x86_64-conda-linux-gnu/sysroot/usr/lib64/libc_nonshared.a:
+
+/home/sus/miniforge3/envs/physics/bin/../x86_64-conda-linux-gnu/sysroot/lib64/ld-linux-x86-64.so.2:
+
+/home/sus/miniforge3/envs/physics/lib/libgcc_s.so:
+
+/home/sus/miniforge3/envs/physics/lib/libgcc_s.so:
+
+/home/sus/miniforge3/envs/physics/lib/libgcc_s.so:
+
+/home/sus/miniforge3/envs/physics/lib/libgcc_s.so.1:
+
+/home/sus/miniforge3/envs/physics/bin/../lib/gcc/x86_64-conda-linux-gnu/14.3.0/libgcc.a:
+
+/home/sus/miniforge3/envs/physics/bin/../lib/gcc/x86_64-conda-linux-gnu/14.3.0/libgcc.a:
+
+/home/sus/miniforge3/envs/physics/bin/../lib/gcc/x86_64-conda-linux-gnu/14.3.0/crtendS.o:
+
+/home/sus/miniforge3/envs/physics/bin/../x86_64-conda-linux-gnu/sysroot/usr/lib/../lib/crtn.o:
