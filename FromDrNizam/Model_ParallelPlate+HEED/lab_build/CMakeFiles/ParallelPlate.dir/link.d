75f9bc2fb488fff338389e1773d0ac2f5088be66
JT Yong <jarrodyong03@gmail.com>
2026-06-12 00:57:06 +0800

    a

---
+++ FromDrNizam/Model_ParallelPlate+HEED/lab_build/CMakeFiles/ParallelPlate.dir/link.d
@@ -0,0 +1,181 @@
+ParallelPlate: \
+  /home/phypc_lab_xmum/miniforge3/envs/physics/bin/../x86_64-conda-linux-gnu/sysroot/usr/lib/../lib/Scrt1.o \
+  /home/phypc_lab_xmum/miniforge3/envs/physics/bin/../x86_64-conda-linux-gnu/sysroot/usr/lib/../lib/crti.o \
+  /home/phypc_lab_xmum/miniforge3/envs/physics/bin/../lib/gcc/x86_64-conda-linux-gnu/14.3.0/crtbeginS.o \
+  CMakeFiles/ParallelPlate.dir/ParallelPlate.C.o \
+  /home/phypc_lab_xmum/miniforge3/envs/physics/lib/libGarfield.so.0.3 \
+  /home/phypc_lab_xmum/miniforge3/envs/physics/lib/libTree.so.6.38.04 \
+  /home/phypc_lab_xmum/miniforge3/envs/physics/lib/libGdml.so.6.38.04 \
+  /home/phypc_lab_xmum/miniforge3/envs/physics/lib/libGeom.so.6.38.04 \
+  /home/phypc_lab_xmum/miniforge3/envs/physics/lib/libXMLIO.so.6.38.04 \
+  /home/phypc_lab_xmum/miniforge3/envs/physics/lib/libGraf3d.so.6.38.04 \
+  /home/phypc_lab_xmum/miniforge3/envs/physics/lib/libGpad.so.6.38.04 \
+  /home/phypc_lab_xmum/miniforge3/envs/physics/lib/libGraf.so.6.38.04 \
+  /home/phypc_lab_xmum/miniforge3/envs/physics/lib/libHist.so.6.38.04 \
+  /home/phypc_lab_xmum/miniforge3/envs/physics/lib/libMatrix.so.6.38.04 \
+  /home/phypc_lab_xmum/miniforge3/envs/physics/lib/libHeed.so \
+  /home/phypc_lab_xmum/miniforge3/envs/physics/lib/libGarfieldRandom.so \
+  /home/phypc_lab_xmum/miniforge3/envs/physics/lib/libMathCore.so.6.38.04 \
+  /home/phypc_lab_xmum/miniforge3/envs/physics/lib/libImt.so.6.38.04 \
+  /home/phypc_lab_xmum/miniforge3/envs/physics/lib/libMultiProc.so.6.38.04 \
+  /home/phypc_lab_xmum/miniforge3/envs/physics/lib/libNet.so.6.38.04 \
+  /home/phypc_lab_xmum/miniforge3/envs/physics/lib/libRIO.so.6.38.04 \
+  /home/phypc_lab_xmum/miniforge3/envs/physics/lib/libThread.so.6.38.04 \
+  /home/phypc_lab_xmum/miniforge3/envs/physics/bin/../x86_64-conda-linux-gnu/sysroot/usr/lib/../lib/libpthread.so \
+  /home/phypc_lab_xmum/miniforge3/envs/physics/bin/../x86_64-conda-linux-gnu/sysroot/usr/lib/../lib/libpthread.so \
+  /home/phypc_lab_xmum/miniforge3/envs/physics/bin/../x86_64-conda-linux-gnu/sysroot/usr/lib/../lib/libpthread.so \
+  /home/phypc_lab_xmum/miniforge3/envs/physics/bin/../x86_64-conda-linux-gnu/sysroot/lib64/libpthread.so.0 \
+  /home/phypc_lab_xmum/miniforge3/envs/physics/bin/../x86_64-conda-linux-gnu/sysroot/usr/lib64/libpthread_nonshared.a \
+  /home/phypc_lab_xmum/miniforge3/envs/physics/lib/libCore.so.6.38.04 \
+  /home/phypc_lab_xmum/miniforge3/envs/physics/lib/libMagboltz.so.11.19 \
+  /home/phypc_lab_xmum/miniforge3/envs/physics/lib/libneBEM.so \
+  /home/phypc_lab_xmum/miniforge3/envs/physics/lib/libDegrade.so.3.17 \
+  /home/phypc_lab_xmum/miniforge3/envs/physics/lib/libgomp.so \
+  /home/phypc_lab_xmum/miniforge3/envs/physics/bin/../x86_64-conda-linux-gnu/sysroot/usr/lib/../lib/libpthread.so \
+  /home/phypc_lab_xmum/miniforge3/envs/physics/bin/../x86_64-conda-linux-gnu/sysroot/usr/lib/../lib/libpthread.so \
+  /home/phypc_lab_xmum/miniforge3/envs/physics/bin/../x86_64-conda-linux-gnu/sysroot/usr/lib/../lib/libpthread.so \
+  /home/phypc_lab_xmum/miniforge3/envs/physics/bin/../x86_64-conda-linux-gnu/sysroot/lib64/libpthread.so.0 \
+  /home/phypc_lab_xmum/miniforge3/envs/physics/bin/../x86_64-conda-linux-gnu/sysroot/usr/lib64/libpthread_nonshared.a \
+  /home/phypc_lab_xmum/miniforge3/envs/physics/lib/libGarfieldGPU.a \
+  /home/phypc_lab_xmum/miniforge3/envs/physics/lib/libstdc++.so \
+  /home/phypc_lab_xmum/miniforge3/envs/physics/bin/../x86_64-conda-linux-gnu/sysroot/usr/lib/../lib/libm.so \
+  /home/phypc_lab_xmum/miniforge3/envs/physics/lib/libgcc_s.so \
+  /home/phypc_lab_xmum/miniforge3/envs/physics/lib/libgcc_s.so \
+  /home/phypc_lab_xmum/miniforge3/envs/physics/lib/libgcc_s.so \
+  /home/phypc_lab_xmum/miniforge3/envs/physics/lib/libgcc_s.so.1 \
+  /home/phypc_lab_xmum/miniforge3/envs/physics/bin/../lib/gcc/x86_64-conda-linux-gnu/14.3.0/libgcc.a \
+  /home/phypc_lab_xmum/miniforge3/envs/physics/bin/../lib/gcc/x86_64-conda-linux-gnu/14.3.0/libgcc.a \
+  /home/phypc_lab_xmum/miniforge3/envs/physics/bin/../x86_64-conda-linux-gnu/sysroot/usr/lib/../lib/libc.so \
+  /home/phypc_lab_xmum/miniforge3/envs/physics/bin/../x86_64-conda-linux-gnu/sysroot/usr/lib/../lib/libc.so \
+  /home/phypc_lab_xmum/miniforge3/envs/physics/bin/../x86_64-conda-linux-gnu/sysroot/usr/lib/../lib/libc.so \
+  /home/phypc_lab_xmum/miniforge3/envs/physics/bin/../x86_64-conda-linux-gnu/sysroot/lib64/libc.so.6 \
+  /home/phypc_lab_xmum/miniforge3/envs/physics/bin/../x86_64-conda-linux-gnu/sysroot/usr/lib64/libc_nonshared.a \
+  /home/phypc_lab_xmum/miniforge3/envs/physics/bin/../x86_64-conda-linux-gnu/sysroot/lib64/ld-linux-x86-64.so.2 \
+  /home/phypc_lab_xmum/miniforge3/envs/physics/lib/libgcc_s.so \
+  /home/phypc_lab_xmum/miniforge3/envs/physics/lib/libgcc_s.so \
+  /home/phypc_lab_xmum/miniforge3/envs/physics/lib/libgcc_s.so \
+  /home/phypc_lab_xmum/miniforge3/envs/physics/lib/libgcc_s.so.1 \
+  /home/phypc_lab_xmum/miniforge3/envs/physics/bin/../lib/gcc/x86_64-conda-linux-gnu/14.3.0/libgcc.a \
+  /home/phypc_lab_xmum/miniforge3/envs/physics/bin/../lib/gcc/x86_64-conda-linux-gnu/14.3.0/libgcc.a \
+  /home/phypc_lab_xmum/miniforge3/envs/physics/bin/../lib/gcc/x86_64-conda-linux-gnu/14.3.0/crtendS.o \
+  /home/phypc_lab_xmum/miniforge3/envs/physics/bin/../x86_64-conda-linux-gnu/sysroot/usr/lib/../lib/crtn.o
+
+/home/phypc_lab_xmum/miniforge3/envs/physics/bin/../x86_64-conda-linux-gnu/sysroot/usr/lib/../lib/Scrt1.o:
+
+/home/phypc_lab_xmum/miniforge3/envs/physics/bin/../x86_64-conda-linux-gnu/sysroot/usr/lib/../lib/crti.o:
+
+/home/phypc_lab_xmum/miniforge3/envs/physics/bin/../lib/gcc/x86_64-conda-linux-gnu/14.3.0/crtbeginS.o:
+
+CMakeFiles/ParallelPlate.dir/ParallelPlate.C.o:
+
+/home/phypc_lab_xmum/miniforge3/envs/physics/lib/libGarfield.so.0.3:
+
+/home/phypc_lab_xmum/miniforge3/envs/physics/lib/libTree.so.6.38.04:
+
+/home/phypc_lab_xmum/miniforge3/envs/physics/lib/libGdml.so.6.38.04:
+
+/home/phypc_lab_xmum/miniforge3/envs/physics/lib/libGeom.so.6.38.04:
+
+/home/phypc_lab_xmum/miniforge3/envs/physics/lib/libXMLIO.so.6.38.04:
+
+/home/phypc_lab_xmum/miniforge3/envs/physics/lib/libGraf3d.so.6.38.04:
+
+/home/phypc_lab_xmum/miniforge3/envs/physics/lib/libGpad.so.6.38.04:
+
+/home/phypc_lab_xmum/miniforge3/envs/physics/lib/libGraf.so.6.38.04:
+
+/home/phypc_lab_xmum/miniforge3/envs/physics/lib/libHist.so.6.38.04:
+
+/home/phypc_lab_xmum/miniforge3/envs/physics/lib/libMatrix.so.6.38.04:
+
+/home/phypc_lab_xmum/miniforge3/envs/physics/lib/libHeed.so:
+
+/home/phypc_lab_xmum/miniforge3/envs/physics/lib/libGarfieldRandom.so:
+
+/home/phypc_lab_xmum/miniforge3/envs/physics/lib/libMathCore.so.6.38.04:
+
+/home/phypc_lab_xmum/miniforge3/envs/physics/lib/libImt.so.6.38.04:
+
+/home/phypc_lab_xmum/miniforge3/envs/physics/lib/libMultiProc.so.6.38.04:
+
+/home/phypc_lab_xmum/miniforge3/envs/physics/lib/libNet.so.6.38.04:
+
+/home/phypc_lab_xmum/miniforge3/envs/physics/lib/libRIO.so.6.38.04:
+
+/home/phypc_lab_xmum/miniforge3/envs/physics/lib/libThread.so.6.38.04:
+
+/home/phypc_lab_xmum/miniforge3/envs/physics/bin/../x86_64-conda-linux-gnu/sysroot/usr/lib/../lib/libpthread.so:
+
+/home/phypc_lab_xmum/miniforge3/envs/physics/bin/../x86_64-conda-linux-gnu/sysroot/usr/lib/../lib/libpthread.so:
+
+/home/phypc_lab_xmum/miniforge3/envs/physics/bin/../x86_64-conda-linux-gnu/sysroot/usr/lib/../lib/libpthread.so:
+
+/home/phypc_lab_xmum/miniforge3/envs/physics/bin/../x86_64-conda-linux-gnu/sysroot/lib64/libpthread.so.0:
+
+/home/phypc_lab_xmum/miniforge3/envs/physics/bin/../x86_64-conda-linux-gnu/sysroot/usr/lib64/libpthread_nonshared.a:
+
+/home/phypc_lab_xmum/miniforge3/envs/physics/lib/libCore.so.6.38.04:
+
+/home/phypc_lab_xmum/miniforge3/envs/physics/lib/libMagboltz.so.11.19:
+
+/home/phypc_lab_xmum/miniforge3/envs/physics/lib/libneBEM.so:
+
+/home/phypc_lab_xmum/miniforge3/envs/physics/lib/libDegrade.so.3.17:
+
+/home/phypc_lab_xmum/miniforge3/envs/physics/lib/libgomp.so:
+
+/home/phypc_lab_xmum/miniforge3/envs/physics/bin/../x86_64-conda-linux-gnu/sysroot/usr/lib/../lib/libpthread.so:
+
+/home/phypc_lab_xmum/miniforge3/envs/physics/bin/../x86_64-conda-linux-gnu/sysroot/usr/lib/../lib/libpthread.so:
+
+/home/phypc_lab_xmum/miniforge3/envs/physics/bin/../x86_64-conda-linux-gnu/sysroot/usr/lib/../lib/libpthread.so:
+
+/home/phypc_lab_xmum/miniforge3/envs/physics/bin/../x86_64-conda-linux-gnu/sysroot/lib64/libpthread.so.0:
+
+/home/phypc_lab_xmum/miniforge3/envs/physics/bin/../x86_64-conda-linux-gnu/sysroot/usr/lib64/libpthread_nonshared.a:
+
+/home/phypc_lab_xmum/miniforge3/envs/physics/lib/libGarfieldGPU.a:
+
+/home/phypc_lab_xmum/miniforge3/envs/physics/lib/libstdc++.so:
+
+/home/phypc_lab_xmum/miniforge3/envs/physics/bin/../x86_64-conda-linux-gnu/sysroot/usr/lib/../lib/libm.so:
+
+/home/phypc_lab_xmum/miniforge3/envs/physics/lib/libgcc_s.so:
+
+/home/phypc_lab_xmum/miniforge3/envs/physics/lib/libgcc_s.so:
+
+/home/phypc_lab_xmum/miniforge3/envs/physics/lib/libgcc_s.so:
+
+/home/phypc_lab_xmum/miniforge3/envs/physics/lib/libgcc_s.so.1:
+
+/home/phypc_lab_xmum/miniforge3/envs/physics/bin/../lib/gcc/x86_64-conda-linux-gnu/14.3.0/libgcc.a:
+
+/home/phypc_lab_xmum/miniforge3/envs/physics/bin/../lib/gcc/x86_64-conda-linux-gnu/14.3.0/libgcc.a:
+
+/home/phypc_lab_xmum/miniforge3/envs/physics/bin/../x86_64-conda-linux-gnu/sysroot/usr/lib/../lib/libc.so:
+
+/home/phypc_lab_xmum/miniforge3/envs/physics/bin/../x86_64-conda-linux-gnu/sysroot/usr/lib/../lib/libc.so:
+
+/home/phypc_lab_xmum/miniforge3/envs/physics/bin/../x86_64-conda-linux-gnu/sysroot/usr/lib/../lib/libc.so:
+
+/home/phypc_lab_xmum/miniforge3/envs/physics/bin/../x86_64-conda-linux-gnu/sysroot/lib64/libc.so.6:
+
+/home/phypc_lab_xmum/miniforge3/envs/physics/bin/../x86_64-conda-linux-gnu/sysroot/usr/lib64/libc_nonshared.a:
+
+/home/phypc_lab_xmum/miniforge3/envs/physics/bin/../x86_64-conda-linux-gnu/sysroot/lib64/ld-linux-x86-64.so.2:
+
+/home/phypc_lab_xmum/miniforge3/envs/physics/lib/libgcc_s.so:
+
+/home/phypc_lab_xmum/miniforge3/envs/physics/lib/libgcc_s.so:
+
+/home/phypc_lab_xmum/miniforge3/envs/physics/lib/libgcc_s.so:
+
+/home/phypc_lab_xmum/miniforge3/envs/physics/lib/libgcc_s.so.1:
+
+/home/phypc_lab_xmum/miniforge3/envs/physics/bin/../lib/gcc/x86_64-conda-linux-gnu/14.3.0/libgcc.a:
+
+/home/phypc_lab_xmum/miniforge3/envs/physics/bin/../lib/gcc/x86_64-conda-linux-gnu/14.3.0/libgcc.a:
+
+/home/phypc_lab_xmum/miniforge3/envs/physics/bin/../lib/gcc/x86_64-conda-linux-gnu/14.3.0/crtendS.o:
+
+/home/phypc_lab_xmum/miniforge3/envs/physics/bin/../x86_64-conda-linux-gnu/sysroot/usr/lib/../lib/crtn.o:
---
+++ FromDrNizam/Model_ParallelPlate+HEED/lab_build/IonPlot.py
@@ -0,0 +1,68 @@
+import ROOT
+import csv
+
+time       = []
+#i_electron = []
+i_ion      = []
+#i_total    = []
+
+with open("ElectronIonCurrent.csv") as f:
+    reader = csv.reader(f)
+    next(reader)  # skip header line
+    next(reader)  # skip "The induced signal." line
+    for row in reader:
+        row = [r.strip() for r in row]
+        if len(row) < 10:
+            continue
+        try:
+            time.append(float(row[0]))
+ #           i_total.append(float(row[3]))
+  #          i_electron.append(float(row[6]))
+            i_ion.append(float(row[9]))
+        except ValueError:
+            continue
+
+n = len(time)
+
+#g_electron = ROOT.TGraph(n)
+g_ion      = ROOT.TGraph(n)
+#g_total    = ROOT.TGraph(n)
+
+for i in range(n):
+ #   g_electron.SetPoint(i, time[i], i_electron[i])
+    g_ion.SetPoint(i,      time[i], i_ion[i])
+  #  g_total.SetPoint(i,    time[i], i_total[i])
+
+# Style
+'''
+g_electron.SetLineColor(ROOT.kBlue);  g_electron.SetLineWidth(2)
+g_electron.SetMarkerColor(ROOT.kBlue); g_electron.SetMarkerStyle(20); g_electron.SetMarkerSize(0.6)
+'''
+g_ion.SetLineColor(ROOT.kRed);  g_ion.SetLineWidth(2)
+g_ion.SetMarkerColor(ROOT.kRed); g_ion.SetMarkerStyle(21); g_ion.SetMarkerSize(0.6)
+'''
+g_total.SetLineColor(ROOT.kBlack); g_total.SetLineWidth(2); g_total.SetLineStyle(2)
+'''
+c = ROOT.TCanvas("c_signal", "Induced Signal", 900, 600)
+c.SetGrid(); c.SetLeftMargin(0.12); c.SetBottomMargin(0.12)
+'''
+g_electron.SetTitle("Induced Current;Time [ns];Current [fC/ns]")
+g_electron.Draw("APL")
+g_electron.GetXaxis().SetRangeUser(time[0], 70.0)
+'''
+g_ion.SetTitle("Induced Ion Current;Time [ns];Current [fC/ns]")
+g_ion.Draw("APL")
+g_ion.GetXaxis().SetRangeUser(time[0], 10000.0)
+#g_ion.Draw("PL SAME")
+#g_total.Draw("L SAME")
+
+#leg = ROOT.TLegend(0.55, 0.65, 0.88, 0.88)
+#leg.SetBorderSize(1)
+#leg.AddEntry(g_electron, "Electron signal", "lp")
+#leg.AddEntry(g_ion,      "Ion signal",      "lp")
+#leg.AddEntry(g_total,    "Total signal",    "l")
+#leg.Draw()
+
+ROOT.gPad.Update()
+c.SaveAs("induced_ion_signal.png")
+print("Saved induced_ion_signal.png")
